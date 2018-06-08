eac46c6e6d79508ea0fe5c9bbbbdf85a3dbf5286
George Brighton <george@gebn.co.uk>
2018-06-08 20:06:41 +0100

    Unencrypt the `TRAVIS_ACCESS_TOKEN` environment variable

--- travis_aws_key_rotate/lambda.py
+++ travis_aws_key_rotate/lambda.py
@@ -1,12 +1,8 @@
 # -*- coding: utf-8 -*-
-import boto3
-import base64
 import logging
 import os
 
 import trawsate
-
-kms = boto3.client('kms')
 
 logger = logging.getLogger(__name__)
 logging.getLogger('boto3').setLevel(logging.INFO)
@@ -15,29 +11,7 @@ logging.getLogger('urllib3').setLevel(logging.INFO)
 logger.setLevel(logging.DEBUG)
 
 
-def _kms_decrypt(ciphertext: str) -> bytes:
-    """
-    Decrypt a value using KMS.
-
-    :param ciphertext: The base64-encoded ciphertext.
-    :return: The plaintext bytestring.
-    """
-    return kms.decrypt(
-        CiphertextBlob=base64.b64decode(ciphertext))['Plaintext']
-
-
-def _kms_decrypt_str(ciphertext: str, encoding: str = 'utf-8') -> str:
-    """
-    Decrypt a piece of text using KMS.
-
-    :param ciphertext: The base64-encoded ciphertext.
-    :param encoding: The encoding of the text.
-    :return: The plaintext.
-    """
-    return _kms_decrypt(ciphertext).decode(encoding)
-
-
-_TRAVIS_ACCESS_TOKEN = _kms_decrypt_str(os.environ['TRAVIS_ACCESS_TOKEN'])
+_TRAVIS_ACCESS_TOKEN = os.environ['TRAVIS_ACCESS_TOKEN']
 
 
 # noinspection PyUnusedLocal
